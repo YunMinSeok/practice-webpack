f72c522e4e7b2db289427e5668ab839a23d693d3
윤민석 <ykmvm147@naver.com>
2022-11-12 15:50:29 +0900

    feat: setting chrome,ie preset env

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  presets: ["@babel/preset-env"],
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          chrome: "79",
+          ie: "11",
+        },
+      },
+    ],
+  ],
 };
