f9bf2af6da19044cb9a5db1208b5698c5125a36b
윤민석 <ykmvm147@naver.com>
2022-11-12 16:01:20 +0900

    feat: Download core-js

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   mode: "development",
   entry: {
-    main: "./src/app.js",
+    main: "./app.js",
   },
   output: {
     path: path.resolve("./dist"),
@@ -32,6 +32,11 @@ module.exports = {
           name: "[name].[ext]?[hash]",
           limit: 20000, //20kb
         },
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
       },
     ],
   },
