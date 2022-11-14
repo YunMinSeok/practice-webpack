a1ccf593cd8463bf4ad620d1c429f1ad82b18572
윤민석 <ykmvm147@naver.com>
2022-11-14 22:22:55 +0900

    feat: Update eslint init config setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,17 @@
 module.exports = {
-  extends: ["eslint:recommended"],
-  //   rules: {
-  //     "no-unexpected-multiline": "error",
-  //     "no-extra-semi": "error",
-  //   },
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
 };
