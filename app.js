42c967757443afae2611ed5b98cdf44e8b8c6dcd
윤민석 <ykmvm147@naver.com>
2022-11-17 21:45:34 +0900

    feat: Download mock api

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-var foo = "";
+import axios from "axios";
 
-console.log();
+document.addEventListener("DOMContentLoaded", async () => {
+  const res = await axios.get("/api/users");
+  console.log(res);
+  document.body.innterHTML = (res.data || [])
+    // eslint-disable-next-line prettier/prettier
+    .map((user) => {
+      return `<div>${user.id}: ${user.name}</div>`;
+    })
+    .join("");
+});
