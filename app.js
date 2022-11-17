5ee17d26b6e430ddf79440a78ec69e4b80d28907
윤민석 <ykmvm147@naver.com>
2022-11-17 21:53:22 +0900

    feat: app.js 오타 수정

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ import axios from "axios";
 document.addEventListener("DOMContentLoaded", async () => {
   const res = await axios.get("/api/users");
   console.log(res);
-  document.body.innterHTML = (res.data || [])
-    // eslint-disable-next-line prettier/prettier
+  document.body.innerHTML = (res.data || [])
     .map((user) => {
       return `<div>${user.id}: ${user.name}</div>`;
     })
