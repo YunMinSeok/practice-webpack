664ee2fbb0b678c6537afe7a8973901155fa776a
윤민석 <ykmvm147@naver.com>
2022-11-20 15:11:49 +0900

    feat: Update app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
-import "./app.css";
-import nyancat from "./nyancat.jpg";
+import form from "./form";
+import result from "./result";
 
-document.addEventListener("DOMContentLoader", () => {
-  document.body.innerHTML = `<img src="${nyancat}" />`;
+document.addEventListener("DOMContentLoader", async () => {
+  const formEl = document.createElement("div");
+  formEl.innerHTML = form.render();
+  document.body.appendChild(formEl);
+
+  resultEl = document.createElement("div");
+  resultEl.innerHTML = await result.render();
+  document.body.appendChild(resultEl);
 });
 
-console.log(process.env.NODE_ENV);
-console.log(api.domain);
+if (module.hot) {
+  console.log("핫 모듈 켜짐");
+
+  module.hot.accept("./result", async () => {
+    console.log("result 모듈 변경됨");
+    resultEl.innerHTML = await result.render();
+  });
+}
